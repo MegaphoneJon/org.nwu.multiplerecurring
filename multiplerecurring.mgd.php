f9dbe4b8d1a7192ccda69158547ba381a703245f
Jon Goldberg <jon@megaphonetech.com>
2018-01-09 17:41:52 -0500

    Create a 'Multiple Recurring Payments' tag on install

---
+++ multiplerecurring.mgd.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+  0 => array(
+    'name' => 'MultipleRecurringTag',
+    'entity' => 'tag',
+    'params' => array(
+      'version' => 3,
+      'name' => 'Multiple Recurring Payments',
+      'color' => '#ff0000',
+    ),
+  ),
+);
